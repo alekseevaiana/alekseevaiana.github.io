ee7d414198ac79a879c4d095f87182088efe6ea2
alekseevayana <alekseevayana@bk.ru>
2021-06-28 13:30:23 -0700

    create couple elements

--- filter_projects.js
+++ filter_projects.js
@@ -1,3 +1,6 @@
 // find buttons with projects
 // push button => appears in new block "filter-projects", projects filtered
 // push button in "filter-project" => button removed, projects filtered
+
+const skills__items = document.querySelectorAll(".skills__item");
+const skills__button = document.querySelectorAll(".skills__button");
